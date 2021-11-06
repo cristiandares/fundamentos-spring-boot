303931407094c3336a0a349611ea9cc88e52fa80
Cristian Restrepo <cristiandares85@hotmail.com>
2021-11-06 18:31:16 -0500

    Uso de properties con ejemplo de generacion de POJO

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,8 +2,11 @@ package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentos.springboot.fundamentos.configuration.MyConfigurationBean;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -16,13 +19,19 @@ public class FundamentosApplication implements CommandLineRunner {
     private ComponentDependency componentDependency;
     private MyBean myBean;
     private MyBeanWithDependency myBeanWithDependency;
+    private MyBeanWithProperties myBeanWithProperties;
+    private UserPojo userPojo;
 
     public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency component,
                                   MyBean myBean,
-                                  MyBeanWithDependency myBeanWithDependency){
+                                  MyBeanWithDependency myBeanWithDependency,
+                                  MyBeanWithProperties myBeanWithProperties,
+                                  UserPojo userPojo){
         this.componentDependency = component;
         this.myBean = myBean;
         this.myBeanWithDependency = myBeanWithDependency;
+        this.myBeanWithProperties = myBeanWithProperties;
+        this.userPojo = userPojo;
     }
 
     public static void main(String[] args) {
@@ -34,5 +43,7 @@ public class FundamentosApplication implements CommandLineRunner {
         componentDependency.saludar();
         myBean.print();
         myBeanWithDependency.printWithDependecy();
+        System.out.println(myBeanWithProperties.function());
+        System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
     }
 }
